0b35a717f3efe65cecaf4b0b582c10cd4fcbb764
fermatmind <17551046983@163.com>
2026-06-02 15:11:41 +0800

    fix(ux): blend homepage HUD background naturally

--- components/marketing/home/HeroHudArtwork.tsx
+++ components/marketing/home/HeroHudArtwork.tsx
@@ -4,7 +4,7 @@ export function HeroHudArtwork() {
   return (
     <div
       aria-hidden="true"
-      className="pointer-events-none relative ml-6 mt-[7px] hidden aspect-[847/558] w-[calc(100%+0.625rem)] self-start overflow-visible lg:block xl:ml-[6.632px] xl:mt-[calc(4.842vw-65px)] xl:w-[56.96vw]"
+      className="pointer-events-none relative ml-6 mt-[7px] hidden aspect-[887/558] w-[calc(100%+0.625rem)] self-start overflow-visible lg:block xl:-ml-[39px] xl:mt-[calc(4.842vw-65px)] xl:w-[59.65vw]"
     >
       <Image
         src="/images/home/hero-assessment-hud.png"
@@ -13,7 +13,7 @@ export function HeroHudArtwork() {
         priority
         unoptimized
         sizes="(min-width: 1280px) 56vw, 52vw"
-        className="object-contain object-right opacity-100"
+        className="object-contain object-right opacity-100 mix-blend-screen brightness-110 contrast-110 [mask-image:linear-gradient(to_right,transparent_0%,rgba(0,0,0,0.1)_8%,rgba(0,0,0,0.72)_21%,black_32%,black_100%)] [-webkit-mask-image:linear-gradient(to_right,transparent_0%,rgba(0,0,0,0.1)_8%,rgba(0,0,0,0.72)_21%,black_32%,black_100%)]"
       />
     </div>
   );
